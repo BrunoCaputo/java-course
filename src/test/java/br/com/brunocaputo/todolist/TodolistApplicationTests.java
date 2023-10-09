cf92761542d62b4d0392519c6f1bfa37aa160a2a
BrunoCaputo <brunopc.dev@gmail.com>
2023-10-09 19:56:50 -0300

    First Commit

---
+++ src/test/java/br/com/brunocaputo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunocaputo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
